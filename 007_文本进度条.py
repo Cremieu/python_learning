db1bddc2ccc857bf5d66845a5259d00f0b69f2ff
Cremieu <lsh12351234@gmail.com>
2020-07-03 13:56:29 +0800

    Update 007_文本进度条.py

--- 007_文本进度条.py
+++ 007_文本进度条.py
@@ -8,4 +8,4 @@ for i in range(scale+1):
     c = (i/scale)*100
     dur = time.perf_counter() - start
     print('\r{:^3.0f}%[{}{}]{:.2f}s'.format(c,a,b,dur),end='')
-    time.sleep(0.1)
+    time.sleep(0.2)
